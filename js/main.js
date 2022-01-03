76bd662d167af7676477a883b4988007a860f101
Fernando Becerra <becerrafernando@gmail.com>
2022-01-03 18:44:16 -0300

    improve dropdown space

--- js/main.js
+++ js/main.js
@@ -387,7 +387,7 @@ Promise.all([
       d3.select(id).style("width", "auto");
       d3.select(id).select(".dropbtn").html(label);
       let prevWidth = d3.select(id).select(".dropbtn").node().getBoundingClientRect().width;
-      d3.select(id).style("width", (prevWidth + 25) + "px");
+      d3.select(id).style("width", (prevWidth + 35) + "px");
     } else {
       d3.select(id).select(".dropbtn").html(label);
     }
